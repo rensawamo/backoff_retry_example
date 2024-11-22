1398fb1e40bd975347ae4e4f190680e0b562e965
rensawamo <ren0112ngzk@gmail.com>
2024-11-22 19:03:52 +0900

    feat: backoff retry

--- cmd/main.go
+++ cmd/main.go
@@ -5,31 +5,70 @@ import (
 	"log"
 	"time"
 
-	
+	pb "backoff_retry_example/gen/buf/proto"
+
+	"github.com/cenkalti/backoff/v4"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+func retryableUnaryCall(client pb.SomeServiceClient, req *pb.SomeRequest) (*pb.SomeResponse, error) {
+	var resp *pb.SomeResponse
+	var err error
+
+	b := backoff.NewExponentialBackOff()
+	b.InitialInterval = 100 * time.Millisecond
+	b.MaxInterval = 2 * time.Second
+	b.MaxElapsedTime = 10 * time.Second
+
+	operation := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		resp, err = client.SomeMethod(ctx, req)
+		if err != nil {
+			st, ok := status.FromError(err)
+			if ok && isRetryableCode(st.Code()) {
+				log.Printf("Retryable error: %v. Retrying...", err)
+				return err
+			}
+			return backoff.Permanent(err)
+		}
+		return nil
+	}
+
+	err = backoff.Retry(operation, b)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func isRetryableCode(code codes.Code) bool {
+	switch code {
+	case codes.Unavailable, codes.DeadlineExceeded:
+		return true
+	default:
+		return false
+	}
+}
+
 func main() {
-	// gRPCサーバへの接続設定
-	conn, err := grpc.Dial("core-service:50051", grpc.WithInsecure())
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect to gRPC server: %v", err)
 	}
 	defer conn.Close()
 
-	client := pb.NewCoreServiceClient(conn)
+	client := pb.NewSomeServiceClient(conn)
 
-	// リクエスト
-	req := &pb.SomeRequest{Data: "example"}
+	req := &pb.SomeRequest{Message: "Hello, gRPC with Backoff!"}
 
-	// 汎用リトライメソッドの呼び出し
-	ctx := context.Background()
-	resp, err := retryableCall(ctx, func(ctx context.Context) (*pb.SomeResponse, error) {
-		return client.SomeMethod(ctx, req) // 呼び出したいgRPCメソッドを指定
-	}, 7*time.Second, 100*time.Millisecond, 2*time.Second)
-
+	resp, err := retryableUnaryCall(client, req)
 	if err != nil {
-		log.Fatalf("Request failed: %v", err)
+		log.Fatalf("Request failed after retries: %v", err)
 	}
 
 	log.Printf("Response received: %v", resp)
